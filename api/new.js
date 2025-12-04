bee12307fe2abffbab2f3d9bba681cd126bcdf1f
SubhamPolaris <subham.das@polariscampus.com>
2025-12-04 23:39:44 +0530

    added gnews apikey

--- api/new.js
+++ api/new.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const r = await fetch(`https://gnews.io/api/v4/search?q=latest&topic=general&lang=en&apikey=YOUR_KEY`);
+  const r = await fetch(`https://gnews.io/api/v4/search?q=latest&topic=general&lang=en&apikey=994ad92756a3d2e963f645d08c268201`);
   const data = await r.json();
   res.status(200).json(data);
 }
